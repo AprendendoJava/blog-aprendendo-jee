9ba960f04d53a3677bc7a2215353b7af82807538
William Siqueira <william.fatecsjc@gmail.com>
2019-03-11 18:35:14 -0300

    Adicionando exemplo feito na live com o JUG Vale

---
+++ microprofile/quarkus-crud-live/src/main/java/org/jugvale/crud/rest/EncontroResource.java
@@ -0,0 +1,56 @@
+package org.jugvale.crud.rest;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jugvale.crud.model.Encontro;
+import org.jugvale.crud.service.EncontroService;
+
+@Path("/encontro")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class EncontroResource {
+
+    @Inject
+    EncontroService encontroService;
+
+    @GET
+    public List<Encontro> lista() {
+        return encontroService.lista();
+    }
+    
+    @PUT
+    public Response cria(Encontro encontro) {
+        long id = encontroService.cria(encontro);
+        return Response.created(URI.create("http://localhost:8080/encontro/" + id))
+                       .build();
+    }
+    
+    @GET
+    @Path("{id}")
+    public Encontro porId(@PathParam("id") long id) {
+        return encontroService.porId(id);
+    }
+    
+    @DELETE
+    @Path("{id}")
+    public void apaga(@PathParam("id") long id) {
+        Encontro encontro = encontroService.porId(id);
+        if (encontro == null) {
+            throw new WebApplicationException(404);
+        }
+        encontroService.apagar(id);
+    }
+}
